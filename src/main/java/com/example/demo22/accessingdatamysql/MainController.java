4a522721f449d6901a8d7f4fcf041e14213f6ade
leetdone <lindong_ye@163.com>
2023-04-09 14:46:46 -0400

    add delete getbyId

--- src/main/java/com/example/demo22/accessingdatamysql/MainController.java
+++ src/main/java/com/example/demo22/accessingdatamysql/MainController.java
@@ -5,13 +5,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-    @Controller // This means that this class is a Controller
+import java.util.Optional;
+
+@Controller // This means that this class is a Controller
     @RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
     public class MainController {
         @Autowired // This means to get the bean called userRepository
@@ -35,6 +35,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
         public @ResponseBody Iterable<User> getAllUsers() {
             // This returns a JSON or XML with the users
             return userRepository.findAll();
+        }
+        @GetMapping(path="/")
+        public @ResponseBody Optional<User> getUserById(@RequestParam String id){
+            return userRepository.findById(Integer.valueOf(id));
+        }
+
+        @GetMapping(path="/delete")
+        public @ResponseBody void deleteUserById(@RequestParam String id){
+            userRepository.deleteById(Integer.valueOf(id));
         }
     }
 
